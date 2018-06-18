07ae35c385b55e29b90f769dec7c9afca48dc507
Jarek <spsp01@o2.pl>
2018-06-18 10:20:18 +0200

    New views

--- toolseo/urls.py
+++ toolseo/urls.py
@@ -21,5 +21,6 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tool.urls')),
+    path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
